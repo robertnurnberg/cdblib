24e755882109801ce7a37033647cad7dc2dd0d2c
Robert Nurnberg @ elitebook <robert.nurnberg@gmx.de>
2023-04-24 09:47:22 +0200

    Change output syntax and ignore text in fen beyond ';'.

--- fens2cdb.py
+++ fens2cdb.py
@@ -13,16 +13,17 @@ cdb = cdblib.cdbAPI()
 unknown = 0
 with open(args.filename) as f:
     for fen in f:
-        if fen and fen.strip():
+        fen = fen.strip()
+        if fen:
             if fen.startswith("#"): continue # ignore comments
+            fen, _, _ = fen.partition(";")   # ignore all beyond first ";"
             r = cdb.queryscore(fen)
             if r.get("status") == "unknown": unknown += 1
             score = cdblib.json2eval(r)
             if score != "" and args.verbose: 
-                if type(score) is int: score = f"{score}cp"
-                if "ply" in r: score += f", ply: {r['ply']}"
-                score = f"({score})"
-            print(f"{fen[:-1]} {score}")
+                if "ply" in r: score = f"{score}, ply: {r['ply']}"
+                score = f"cdb eval: {score}"
+            print(f"{fen}; {score};")
 
 if unknown:
     print(f"# The file {args.filename} contained {unknown} new chessdb.cn positions.")
